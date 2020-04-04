2f5d614a8747d01b35578661e19a6a76432b04c8
253257 <253257@student.pwr.edu.pl>
2020-04-04 16:11:06 +0200

    utworzenie lab2

---
+++ lab2/menu.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<iostream>
+#include"tablice.h"
+using namespace std;
+void menu();
---
+++ lab2/tablice.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+using namespace std;
+double* tworzenie_tablicy(size_t n);
+void zmien_wartosc(double* tab, size_t n, double wartosc, size_t index);
+void wyswietl(double* tab, size_t n);
+double* zmiana_rozmiaru_tablicy(double* tab, size_t n, size_t newn);
+
